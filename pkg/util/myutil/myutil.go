fea3b7a6c8f87cc4a8d17497ac7bd2c6d48db255
ke1nys <1478456309@qq.com>
2024-04-30 22:07:28 +0800

    init commit

---
+++ pkg/util/myutil/myutil.go
@@ -0,0 +1,45 @@
+package myutil
+
+import (
+	"fmt"
+	"github.com/fatedier/frp/pkg/msg"
+	"math/rand"
+	"time"
+)
+
+func GetMessage(serverIp string, remoteIp string, loginMsg *msg.Login) string {
+	msg := ""
+	//	msg := fmt.Sprintf(`客户端信息
+	//remote ip: %v
+	//local ip: %v
+	//hostname: %v
+	//os: %v
+	//arch: %v
+	//username: %v
+	//version: %v`, remoteIp, loginMsg.Ip, loginMsg.HostName, loginMsg.Os, loginMsg.Arch, loginMsg.UserName, loginMsg.Version)
+
+	if loginMsg.SocksPort != 0 {
+		msg += "\n\n"
+		msg += fmt.Sprintf(`socks 代理信息`)
+		if loginMsg.SocksUser != "" && loginMsg.SocksPass != "" {
+			msg += "\n"
+			msg += fmt.Sprintf(`user: %v
+pass: %v`, loginMsg.SocksUser, loginMsg.SocksPass)
+		}
+	}
+	return msg
+}
+
+func RandStr(length int) string {
+	dicts := []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	result := make([]byte, length)
+
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	for i := 0; i < length; i++ {
+		result[i] = dicts[r.Intn(len(dicts))]
+	}
+
+	return string(result)
+}
